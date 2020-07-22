12ddf5716f8f23dc3f0fe1c3c4e15a546032f45d
CiciIrmayanti <55769697+CiciIrmayanti@users.noreply.github.com>
2020-07-22 22:23:57 +0700

    Project-Laravel-1

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title> Home </title>
+</head>
+<style>
+    .header {
+        width: 100%;
+        height: 100px;
+        background: #40407a;
+        text-align: center;
+    }
+
+    .header h1 {
+        line-height: 100px;
+        font-family: Lucida Calligraphy;
+        color: white;
+    }
+
+    #main-content {
+        height: 400px;
+        background: #706fd3;
+    }
+
+    #right-sidebar {
+        height: 200px;
+        background: #40407a;
+        font-family: Lucida Calligraphy;
+        color: white;
+        font-size: 15px;
+
+    }
+
+    .gambar {
+        margin-left: 35px;
+    }
+
+    .lingkaran1 {
+        width: 200px;
+        height: 200px;
+        background: #474787;
+        border-radius: 100%;
+        margin-left: 450px;
+    }
+
+    .nama {
+        font-family: Lucida Calligraphy;
+        font-size: 25px;
+        color: white;
+    }
+    body {
+        font-family: Lucida Calligraphy;
+
+    }
+</style>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <h1> Profile </h1>
+        </div>
+    </div>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">My Blog</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="utama">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="profil" style="color:black;">Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="pengalaman" style="color:black;">Experience</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="porto" style="color:black;">Fortofolio</a>
+                    </li>
+        </nav>
+    </div>
+    <div class="container">
+        <div id="main-content" class="col-sm-12">
+            <br>
+            <br>
+            <br>
+            <div class="lingkaran1">
+                <br>
+                <img src="asset/woman.png" alt="" class="gambar">
+            </div>
+            <p class="nama" align="center"> <marquee behavior="" direction=""> Cici Irma Yanti </marquee> </p></p>
+            <h5  align="center" style="color:white;"> Student | Progammer </h5>
+
+        </div>
+        <div id="right-sidebar" class="col-sm-12">
+            <br>
+            <table border="0px;" width="500" align="center">
+                <tr>
+                    <td> Nama Lengkap </td>
+                    <td> Cici Irma Yanti </td>
+                </tr>
+                <tr>
+                    <td> Kelas </td>
+                    <td> XII RPL 2 </td>
+                </tr>
+                <tr>
+                    <td> Asal Sekolah </td>
+                    <td> SMK Taruna Bhakti </td>
+                </tr>
+                <tr>
+                    <td> Jurusan </td>
+                    <td> Rekayasa Perangkat Lunak </td>
+                </tr>
+                <tr>
+                    <td> Tempat, Tanggal Lahir </td>
+                    <td> Pekanbaru, 17 Juni 2002 </td>
+                </tr>
+                <tr>
+                    <td> Agama </td>
+                    <td> Islam </td>
+                </tr>
+                <tr>
+                    <td> Jenis Kelamin </td>
+                    <td> Perempuan </td>
+                </tr>
+            </table>
+            </tr>
+
+        </div>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ resources/views/portofolio.blade.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title> Portofolio </title>
+</head>
+<style>
+    .header {
+        width: 100%;
+        height: 100px;
+        background: #40407a;
+        text-align: center;
+    }
+
+    .header h1 {
+        line-height: 100px;
+        font-family: Lucida Calligraphy;
+        color: white;
+    }
+
+    #main-content {
+        height: 400px;
+        background: #706fd3;
+    }
+
+    #right-sidebar {
+        height: 200px;
+        background: #40407a;
+        font-family: Lucida Calligraphy;
+        color: white;
+        font-size: 15px;
+
+    }
+
+    .gambar {
+        margin-left: 35px;
+    }
+
+    .lingkaran1 {
+        width: 200px;
+        height: 200px;
+        background: #474787;
+        border-radius: 100%;
+        margin-left: 450px;
+    }
+
+    .nama {
+        font-family: Lucida Calligraphy;
+        font-size: 25px;
+        color: white;
+    }
+    body {
+        font-family: Lucida Calligraphy;
+
+    }
+</style>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <h1> Portofolio </h1>
+        </div>
+    </div>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">My Blog</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="utama">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="profil" style="color:black;">Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="pengalaman" style="color:black;">Experience</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="porto" style="color:black;">Fortofolio</a>
+                    </li>
+        </nav>
+    </div>
+    <div class="container">
+        <div id="main-content" class="col-sm-12">
+            <br>
+            <br>
+            <br>
+            <div class="lingkaran1">
+                <br>
+                <img src="asset/woman.png" alt="" class="gambar">
+            </div>
+            <p class="nama" align="center"> <marquee behavior="" direction=""> Cici Irma Yanti </marquee> </p>
+            <h5  align="center" style="color:white;"> Student | Progammer </h5>
+
+        </div>
+        <div id="right-sidebar" class="col-sm-12">
+        <br>
+            <table border="0px" align="center">
+                <tr>
+                    <td> <img src="asset/uji.png" alt="" height="150px"> <br> Tugas Uji Level</td>
+                    <td> <img src="asset/coding.png" alt="" height="150px"> <br> Tugas DiCoding</td>
+                    <td> <img src="asset/webb.png" alt="" height="150px"> <br> Tugas Function</td>
+                    <td> <img src="asset/srt.png" alt="" height="150px"> Sertifikat DiCoding</td>
+                </tr>
+
+            </table>
+
+
+
+        </div>
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
